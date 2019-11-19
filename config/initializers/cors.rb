02da321caeab10c9d71944748ce1062d9874b242
github username <github email address>
2019-11-19 11:02:28 -0500

    allow access from other domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@
 ## do the following:   origins '*' - NOT RECCOMENDED TO KEEP IT LIKE THIS )
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3001'
 
     resource '*',
       headers: :any,
