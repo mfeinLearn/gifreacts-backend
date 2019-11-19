cf9f7c5db033465c7790d9fb72aeb1f6a373e0c7
github username <github email address>
2019-11-19 10:40:19 -0500

    add change to origins to white-list who can access api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# we are saying her are the request that we are going to authorize
+## and you can white list them - if you know who you are going to accept
+## request from then you white list them here if you dont know (having
+## a open api meaning you can accept request from everyone then you can
+## do the following:   origins '*' - NOT RECCOMENDED TO KEEP IT LIKE THIS )
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+# probably dont want people to do all of this:
+## methods: [:get, :post, :put, :patch, :delete, :options, :head]
