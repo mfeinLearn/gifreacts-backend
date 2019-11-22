20d764b5cb18eb2bdc9a6a7a2f65ada8153f8f08
github username <github email address>
2019-11-22 11:48:42 -0500

    add changes to the routes and namespaces humer_types

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :humer_types
+  post "/api/v1/gifs", to: "api/v1/gifs#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # I want my api to be gifracts.com/api/v1/resource
   namespace :api do
     namespace :v1 do
       resources :emotions
       resources :gifs
+      resources :humer_types
     end
   end
 end
