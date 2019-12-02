2ca5e97c3292a0958287fa41f0ec924d02733973
github username <github email address>
2019-12-02 01:42:05 -0500

    add all standard actions

--- app/controllers/api/v1/emotions_controller.rb
+++ app/controllers/api/v1/emotions_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::EmotionsController < ApplicationController
+  def index
+    @emotions = Emotion.all
+    render json: @emotions
+  end
+
+  def create
+    @emotion = Emotion.new(emotion_params)
+    if @emotion.save
+      render json: @emotion
+    else
+      render json: {error: 'Error creating emotion'}
+    end
+  end
+
+  def show
+    @emotion = Emotion.find(params[:id])
+    render json: @emotion
+  end
+
+  def destroy
+    @emotion = Emotion.find(params[:id])
+    @emotion.destroy
+  end
+
+  def update
+    @emotion = Emotion.find(params[:id])
+    @emotion.update(name: params["name"])
+    @emotion.save
+    render json: @emotion
+  end
+
+  private
+
+  def emotion_params
+    params.require(:emotion).permit(:name)
+  end
+
 end
