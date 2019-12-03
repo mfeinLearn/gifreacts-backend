3c6fd9ea1c532594692a6533e7f8ab9b0047bef0
github username <github email address>
2019-12-03 12:20:10 -0500

    add changes to add the new custom attributes

--- app/serializers/gif_serializer.rb
+++ app/serializers/gif_serializer.rb
@@ -4,4 +4,18 @@ class GifSerializer
   belongs_to :humer_type
   belongs_to :emotion
 
+  attribute :humer_type_range do |object|
+    # byebug
+    if object.humer_type
+      object.humer_type.range
+    end
+  end
+
+  attribute :emotion_name do |object|
+    # byebug
+    if object.emotion
+      object.emotion.name
+    end
+  end
+
 end
