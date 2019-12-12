fd672715920e5b7d3943946aa928c8315800ef8a
github username <github email address>
2019-12-12 06:55:20 -0500

    add validation to humer type range attribute

--- app/models/humer_type.rb
+++ app/models/humer_type.rb
@@ -3,5 +3,7 @@ class HumerType < ApplicationRecord
   has_many :emotions, through: :gifs
 
   validates :range, presence: true
+  validates :range, :inclusion => { :in => 1..5 }
+  # , :message => "Must be between 1 to 5"
 
 end
